61ee0ec7d6d65b12950abb0a5be5fee82dc48508
denn-p <dennielysp@gmail.com>
2026-03-11 12:11:35 +0000

    creacion-de-home

---
+++ src/front/components/PomodoroZone.jsx
@@ -0,0 +1,8 @@
+export const PomodoroZone = () => {
+    return (
+        <div className="pomodoro-zone">
+            <h2>Pomodoro Zone</h2>
+            <p>Aquí puedes agregar tu temporizador Pomodoro y música.</p>
+        </div>
+    );
+}
