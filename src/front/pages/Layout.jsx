c394da1ce09b81a5a3bce1a90bfd6e81dc6d3679
denn-p <dennielysp@gmail.com>
2026-03-18 12:21:48 +0000

    cambios-importantes

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,10 +1,29 @@
-import { Outlet } from "react-router-dom/dist"
-import ScrollToTop from "../components/ScrollToTop"
-import { Navbar } from "../components/Navbar"
-import { Footer } from "../components/Footer"
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom/dist";
+import ScrollToTop from "../components/ScrollToTop";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import useGlobalReducer from "../hooks/useGlobalReducer";
+import { getProfile } from "../services/loginBS";
 
 export const Layout = () => {
+    const { dispatch } = useGlobalReducer();
     const isMobile = window.innerWidth <= 767;
+
+    useEffect(() => {
+        const loadUser = async () => {
+            const token = localStorage.getItem("token");
+            if (!token) return;
+            const user = await getProfile();
+            if (user) {
+                dispatch({ type: "set_user", payload: user });
+            } else {
+                localStorage.removeItem("token");
+                dispatch({ type: "logout" });
+            }
+        };
+        loadUser();
+    }, []);
 
     return (
         <ScrollToTop>
@@ -26,5 +45,5 @@ export const Layout = () => {
                 <Footer />
             </div>
         </ScrollToTop>
-    )
-}
+    );
+};
